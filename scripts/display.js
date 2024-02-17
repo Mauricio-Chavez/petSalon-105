2d6dc513ede135d5799bb85ea4b41ec0f22cece6
Mauricio-Chavez <mau_chavez003@hotmail.com>
2024-02-17 00:11:33 -0800

    Corrections

--- scripts/display.js
+++ scripts/display.js
@@ -75,7 +75,7 @@ function searchFunction() {
     let tr = table.getElementsByTagName('tr');
 
     for (let i = 0; i < tr.length; i++) {
-        let td = tr[i].getElementsByTagName('td')[0];
+        let td = tr[i].getElementsByTagName('td')[1];
         if (td) {
             let txtValue = td.textContent || td.innerText;
             if (txtValue.toUpperCase().indexOf(filter) > -1) {
